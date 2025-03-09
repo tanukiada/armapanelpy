386fb129349a44a423b5c3b3c0da33de5e7004bd
Ada Taylor <jtaylord17@gmail.com>
2025-03-09 18:43:18 -0500

    added drop down

--- panel.py
+++ panel.py
@@ -1,11 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 import psutil
+import os
 
 ARMA_PATH = "c:\\arma3" # set this to where your arma 3 install is
 ARMA_EXE = "arma3server_x64.exe" # don't change this unless you want 32 bit arma for some reason
 modString = "" # global variable for the modstring, empty by default
 
+def GetModLists():
+    modList = []
+    for file in os.listdir(f"{ARMA_PATH}\\presets"):
+        modList.append(file)
+        return modList
+        
 def GetProcessId():
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == ARMA_EXE:
@@ -19,11 +26,14 @@ def StopServer():
     process = psutil.Process(pid)
     process.kill()
     
-
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Arma Server Panel").grid(column=0, row=0)
-ttk.Button(frm, text="Start Server", command=StartServer).grid(column=0, row=1)
-ttk.Button(frm, text="Stop Server", command=StopServer).grid(column=1, row=1)
+ttk.Button(frm, text="Start Server", command=StartServer).grid(column=0, row=2)
+ttk.Label(frm, text="Select Mod List: ").grid(column=0, row=1)
+combobox = ttk.Combobox(frm, state="readonly")
+combobox['values'] = GetModLists()
+combobox.grid(column=1, row=1)
+ttk.Button(frm, text="Stop Server", command=StopServer).grid(column=2, row=2)
 root.mainloop()
