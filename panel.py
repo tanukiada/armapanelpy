34b0ba74fbb2f9f75b4e4e4da7e1eaba4136a71e
Ada Taylor <jtaylord17@gmail.com>
2025-03-10 17:07:18 -0500

    more changes to set up updater

--- panel.py
+++ panel.py
@@ -1,11 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+from dotenv import load_dotenv
+import requests
 import psutil
+import json
 import os
 
+load_dotenv()
+
+USER_NAME = os.getenv("USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
 ARMA_PATH = "c:\\arma3" # set this to where your arma 3 install is
 ARMA_EXE = "arma3server_x64.exe" # don't change this unless you want 32 bit arma for some reason
-modString = "" # global variable for the modstring, empty by default
 
 def GetModLists():
     modList = []
@@ -18,6 +24,52 @@ def GetProcessId():
         if proc.info['name'] == ARMA_EXE:
             return proc.info['pid']
 
+def GetAllMods():
+    mods = []
+    for fname in os.listdir(path=ARMA_PATH):
+        if fname.startswith("@"):
+            mods.append(fname)
+    return mods
+
+def MakeKeyValueForMetaFile(mod):
+    fileKeyValue = {}
+    with open(f"{ARMA_PATH}\\{mod}\\meta.cpp") as f:
+        fileContent = f.read()
+    key_values = fileContent.splitlines()
+    for key_value in key_values:
+        key, value = key_value.split("=")
+        key = key.strip()
+        value = value.strip()
+        fileKeyValue[key] = value
+    return fileKeyValue
+    
+def GetModId(mod):
+    fileKeyValue = MakeKeyValueForMetaFile(mod)
+    return fileKeyValue['publishedid'].strip(";")
+
+def GetRemoteTimestamp(modID):
+    body = {
+        'itemcount': 1,
+        'publishedfileids[0]': modID
+    }
+    steamRequest = requests.post('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/', data=body)
+    return steamRequest
+
+def GetLocalTimestamp(mod):
+    fileKeyValue = MakeKeyValueForMetaFile(mod)
+    return fileKeyValue['timestamp']
+
+def UpdateAllMods():
+    mods = GetAllMods()
+    for mod in mods:
+        modID = GetModId(mod)
+        UpdateTimestampRemote = GetRemoteTimestamp(modID)
+        jsonContent = UpdateTimestampRemote.json()
+        jsonContent = jsonContent["response"]
+        jsonContent = jsonContent["publishedfiledetails"]
+        remoteTime = jsonContent[0]["time_updated"]
+        localTime = GetLocalTimestamp(mod)
+        
 def StartServer(combobox):
     with open(f"{ARMA_PATH}\\presets\\{combobox.get()}", encoding="utf-8") as f:
         modString = f.read()
@@ -29,6 +81,7 @@ def StopServer():
     process.kill()
     
 root = Tk()
+root.resizable(False, False)
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Arma Server Panel").grid(column=0, row=0)
@@ -37,5 +90,7 @@ combobox = ttk.Combobox(frm, state="readonly")
 combobox['values'] = GetModLists()
 combobox.grid(column=1, row=1)
 ttk.Button(frm, text="Start Server", command=lambda: StartServer(combobox)).grid(column=0, row=2)
-ttk.Button(frm, text="Stop Server", command=StopServer).grid(column=2, row=2)
+ttk.Button(frm, text="Stop Server", command=StopServer).grid(column=1, row=2)
+ttk.Separator(frm, orient='horizontal').grid(column=0, row=3, columnspan=3, sticky='ew')
+ttk.Button(frm, text="Update Mods", command=UpdateAllMods).grid(column=0, row=4)
 root.mainloop()
